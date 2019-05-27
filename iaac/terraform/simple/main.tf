994295208e654e2e1878307da0f3c59102ef09f5
Oliver Rasek <rasekoli@gmail.com>
2019-05-27 17:07:16 +0000

    Using variables for all the settings

--- iaac/terraform/simple/main.tf
+++ iaac/terraform/simple/main.tf
@@ -6,10 +6,10 @@ provider "aws" {
 
 resource "aws_instance" "web" {
 	ami		= "${lookup(var.ubuntu_ami, var.region)}"
-	instance_type	= "t2.micro"
-	key_name	= "aws-keypair"
-	vpc_security_group_ids = ["sg-0722166403e098c7e"] 
+	instance_type	= "${var.instance_type}"
+	key_name	= "${var.key_name}"
+	vpc_security_group_ids = "${var.vpc_security_group_ids}" 
 	tags {
-		Name	= "by-terraform" 
+		Name	= "${var.tag_name}" 
 	}
 }
--- iaac/terraform/simple/variables.tf
+++ iaac/terraform/simple/variables.tf
@@ -3,6 +3,9 @@ variable "shared_credentials_file"	{ default = "/home/ubuntu/.aws/credentials" }
 variable "profile"			{ default = "terraform" }
 variable "ubuntu_ami"			{ default = { 
 						"eu-central-1"	= "ami-090f10efc254eaf55"
-						"eu-west-1"	= "ami-f90a4880"
 						}
 }
+variable "instance_type"		{ default = "t2.micro" }
+variable "key_name"			{ default = "aws-keypair" }
+variable "vpc_security_group_ids"	{ default = ["sg-0722166403e098c7e"] }
+variable "tag_name"			{ default = "terraform" }
